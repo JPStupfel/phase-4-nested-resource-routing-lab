d6e7db0fcdc89caa54d2d6558bb418048a2e8af9
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-07-29 14:03:56 -0500

    commit tree 92538ebec31f0b9ffe2528aa4a86b63dc25d092f

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,41 @@
 class ItemsController < ApplicationController
 
+rescue_from ActiveRecord::RecordNotFound, with: :render_no_find
+
   def index
+    
+    if params[:user_id]
+      user = User.find params[:user_id]
+      items = user.items
+    else
     items = Item.all
+    end
+
     render json: items, include: :user
+  end
+
+  def show
+    items = Item.find_by! id: params[:id]
+    render json: items
+
+  end
+
+  def create
+    user = User.find_by params[:user_id]
+    item = Item.create itemsParams
+    item.user = user
+    render json: item, status: :created
+  end
+
+  private
+  def itemsParams
+    params.permit :name, :description, :price, :user_id
+  end
+
+  def render_no_find error
+    #  byebug
+    #  json: error.message, status: :not_found
+     head :not_found
   end
 
 end
